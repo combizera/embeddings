f379ca91779ffdeb114fd6c3ea1f043deb1f792e
combizera <ycombi349@gmail.com>
2025-06-17 17:06:23 -0300

    feat: comparing words

--- embeddings.js
+++ embeddings.js
@@ -1,5 +1,13 @@
-import { pipeline } from '@xenova/transformers';
+import { pipeline, cos_sim } from '@xenova/transformers';
 
 const embedder = await pipeline("feature-extraction", "Xenova/all-MiniLM-L6-v2");
 
-console.log(await embedder('Dog is funny', { pooling: "mean", normalize: true }))
+async function embedText(text) {
+  return embedder(text, { pooling: "mean", normalize: true }).then(tensor => tensor.tolist()[0]);
+}
+
+console.log(cos_sim(await embedText("Hello"), await embedText("Hi")));
+console.log('---')
+console.log(cos_sim(await embedText("King"), await embedText("Queen")));
+console.log(cos_sim(await embedText("King"), await embedText("Woman")));
+console.log(cos_sim(await embedText("King"), await embedText("Men")));
